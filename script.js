8cfcf767a2592f0a949c15e388782ec2800b2325
TarasYarovyi <yarovoy.taras@gmail.com>
2023-06-26 21:56:27 +0200

    count() updated

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 "use strict";
 
 const stopwatch = document.querySelector(".stopwatch");
-const start = document.querySelector(".pause");
+const start = document.querySelector(".start");
 
-addEventListener("click", count);
+start.addEventListener("click", count);
 
 function count() {
   const startTime = new Date();
   setInterval(() => {
     const currentTime = new Date();
     const diff = new Date(currentTime - startTime);
-    const ms = Math.trunc(diff.getMilliseconds() / 10);
-    const sec = diff.getSeconds();
-    const min = diff.getMinutes();
-
+    const min = diff.getMinutes().toString().padStart(2, 0);
+    const sec = diff.getSeconds().toString().padStart(2, 0);
+    const ms = Math.trunc(diff.getMilliseconds() / 10)
+      .toString()
+      .padStart(2, 0);
     stopwatch.textContent = `${min}:${sec}:${ms}`;
-  }, 100);
+  });
 }
