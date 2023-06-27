a62b753524c7581f31d96354eeada9d1f1562ba6
TarasYarovyi <yarovoy.taras@gmail.com>
2023-06-27 20:13:44 +0200

    count() updated

--- script.js
+++ script.js
@@ -2,19 +2,40 @@
 
 const stopwatch = document.querySelector(".stopwatch");
 const start = document.querySelector(".start");
+const pauseBtn = document.querySelector(".pause");
+let status;
+let startTime;
+let currentTime;
+let diff;
+let min;
+let sec;
+let ms;
 
-start.addEventListener("click", count);
+start.addEventListener("click", on);
+pauseBtn.addEventListener("click", pause);
 
-function count() {
-  const startTime = new Date();
+function on() {
+  startTime = new Date();
+  status = "switchedOff";
+
   setInterval(() => {
-    const currentTime = new Date();
-    const diff = new Date(currentTime - startTime);
-    const min = diff.getMinutes().toString().padStart(2, 0);
-    const sec = diff.getSeconds().toString().padStart(2, 0);
-    const ms = Math.trunc(diff.getMilliseconds() / 10)
-      .toString()
-      .padStart(2, 0);
-    stopwatch.textContent = `${min}:${sec}:${ms}`;
+    if (status === "switchedOff") {
+      currentTime = new Date();
+
+      diff = new Date(currentTime - startTime);
+      min = diff.getMinutes().toString().padStart(2, 0);
+      sec = diff.getSeconds().toString().padStart(2, 0);
+      ms = Math.trunc(diff.getMilliseconds() / 10)
+        .toString()
+        .padStart(2, 0);
+      stopwatch.textContent = `${min}:${sec}:${ms}`;
+    } else if (status === "paused") {
+      startTime = currentTime;
+      stopwatch.textContent = `${min}:${sec}:${ms}`;
+    }
   });
+}
+
+function pause() {
+  status = "paused";
 }
