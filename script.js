84a51d710cd6578b8de280aa8ec0cbc6798a042d
TarasYarovyi <yarovoy.taras@gmail.com>
2023-06-29 16:28:03 +0200

    on(), count() : updated

--- script.js
+++ script.js
@@ -15,71 +15,40 @@ let ms = 0;
 start.addEventListener("click", on);
 pauseBtn.addEventListener("click", pause);
 
-function turnOn() {
-  status = "switchedOn";
-  setInterval(() => {
-    if (ms < 1000) {
-      ms++;
-    } else if (min < 60) {
-      sec++;
-      ms = 0;
-    } else {
-      min++;
-      sec = 0;
-      ms = 0;
-    }
-
-    stopwatch.textContent = `${min}:${sec}:${ms}`;
-  });
-}
-
 function on() {
   if (status === "switchedOn") {
     return;
   } else if (status === "switchedOff") {
     status = "switchedOn";
     startTime = new Date();
-    setInterval(() => {
-      if (status === "switchedOn") {
-        currentTime = new Date();
-
-        diff = new Date(currentTime - startTime);
-        min = diff.getMinutes().toString().padStart(2, 0);
-        sec = diff.getSeconds().toString().padStart(2, 0);
-        ms = Math.trunc(diff.getMilliseconds() / 10)
-          .toString()
-          .padStart(2, 0);
-        stopwatch.textContent = `${min}:${sec}:${ms}`;
-      } else if (status === "paused") {
-        pausedTime = diff;
-
-        return;
-      }
-    });
+    count();
   } else if (status === "paused") {
     status = "switchedOn";
     startTime = new Date();
     startTime.setTime(startTime.getTime() - pausedTime.getTime());
-
-    setInterval(() => {
-      if (status === "switchedOn") {
-        currentTime = new Date();
-
-        diff = new Date(currentTime - startTime);
-        min = diff.getMinutes().toString().padStart(2, 0);
-        sec = diff.getSeconds().toString().padStart(2, 0);
-        ms = Math.trunc(diff.getMilliseconds() / 10)
-          .toString()
-          .padStart(2, 0);
-        stopwatch.textContent = `${min}:${sec}:${ms}`;
-      } else if (status === "paused") {
-        pausedTime = diff;
-        return;
-      }
-    });
+    count();
   }
 }
 
 function pause() {
   status = "paused";
+}
+
+function count() {
+  setInterval(() => {
+    if (status === "switchedOn") {
+      currentTime = new Date();
+
+      diff = new Date(currentTime - startTime);
+      min = diff.getMinutes().toString().padStart(2, 0);
+      sec = diff.getSeconds().toString().padStart(2, 0);
+      ms = Math.trunc(diff.getMilliseconds() / 10)
+        .toString()
+        .padStart(2, 0);
+      stopwatch.textContent = `${min}:${sec}:${ms}`;
+    } else if (status === "paused") {
+      pausedTime = diff;
+      return;
+    }
+  });
 }
