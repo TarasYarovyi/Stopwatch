bc4f1642f325ef31ba8feb6a237a8181d7a5aa44
TarasYarovyi <yarovoy.taras@gmail.com>
2023-07-03 20:09:19 +0200

    lap information and  showTime() added;

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ const stopwatch = document.querySelector(".stopwatch");
 const startBtn = document.querySelector(".start");
 const stopBtn = document.querySelector(".stop");
 const pauseBtn = document.querySelector(".pause");
+const time = document.querySelector(".time");
 
 let status = "switchedOff";
 let startTime;
+let startLap;
 let currentTime;
 let pausedTime;
 let demonstratedTime;
+let displayTime;
 
 startBtn.addEventListener("click", switchStopwatch);
 pauseBtn.addEventListener("click", pause);
@@ -18,10 +21,15 @@ stopBtn.addEventListener("click", stop);
 function switchStopwatch() {
   if (status === "switchedOn") {
     replaceClass(startBtn.firstChild, "fa-play", "fa-arrows-spin");
+    time.style.visibility = "visible";
+    const lap = new Date(currentTime - startLap);
+    showTime(lap, time);
+    startLap = new Date();
   } else if (status === "switchedOff") {
     status = "switchedOn";
     replaceClass(startBtn.firstChild, "fa-play", "fa-arrows-spin");
     startTime = new Date();
+    startLap = startTime;
     count();
   } else if (status === "paused") {
     status = "switchedOn";
@@ -34,7 +42,9 @@ function switchStopwatch() {
 
 function stop() {
   status = "switchedOff";
+  replaceClass(startBtn.firstChild, "fa-arrows-spin", "fa-play");
   stopwatch.textContent = "00:00:00";
+  time.textContent = "00:00:00";
 }
 function pause() {
   status = "paused";
@@ -45,23 +55,24 @@ function count() {
   setInterval(() => {
     if (status === "switchedOn") {
       currentTime = new Date();
-
       demonstratedTime = new Date(currentTime - startTime);
-      const min = demonstratedTime.getMinutes().toString().padStart(2, 0);
-      const sec = demonstratedTime.getSeconds().toString().padStart(2, 0);
-      const ms = Math.trunc(demonstratedTime.getMilliseconds() / 10)
-        .toString()
-        .padStart(2, 0);
-      stopwatch.textContent = `${min}:${sec}:${ms}`;
+      showTime(demonstratedTime, stopwatch);
     } else if (status === "paused") {
       pausedTime = demonstratedTime;
       return;
     }
   });
 }
+function showTime(date, element) {
+  const min = date.getMinutes().toString().padStart(2, 0);
+  const sec = date.getSeconds().toString().padStart(2, 0);
+  const ms = Math.trunc(date.getMilliseconds() / 10)
+    .toString()
+    .padStart(2, 0);
+  element.textContent = `${min}:${sec}:${ms}`;
+}
 
 function replaceClass(element, oldClass, newClass) {
   element.classList.remove(oldClass);
   element.classList.add(newClass);
-  console.log(element);
 }
