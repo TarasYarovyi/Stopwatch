eb5807637544836a76e4752366214405434ddfde
TarasYarovyi <yarovoy.taras@gmail.com>
2023-07-04 19:21:57 +0200

    timeList added; showTime() and  stop() changed;

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const stopwatch = document.querySelector(".stopwatch");
 const startBtn = document.querySelector(".start");
 const stopBtn = document.querySelector(".stop");
 const pauseBtn = document.querySelector(".pause");
-const time = document.querySelector(".time");
+const timeList = document.querySelector(".time-list");
 
 let status = "switchedOff";
 let startTime;
@@ -13,6 +13,7 @@ let currentTime;
 let pausedTime;
 let demonstratedTime;
 let displayTime;
+let lapNumber = 1;
 
 startBtn.addEventListener("click", switchStopwatch);
 pauseBtn.addEventListener("click", pause);
@@ -21,9 +22,12 @@ stopBtn.addEventListener("click", stop);
 function switchStopwatch() {
   if (status === "switchedOn") {
     replaceClass(startBtn.firstChild, "fa-play", "fa-arrows-spin");
-    time.style.visibility = "visible";
     const lap = new Date(currentTime - startLap);
-    showTime(lap, time);
+    let li = document.createElement("li");
+
+    showTime(li, `lap ${lapNumber} - `, lap);
+    lapNumber++;
+    timeList.prepend(li);
     startLap = new Date();
   } else if (status === "switchedOff") {
     status = "switchedOn";
@@ -43,8 +47,9 @@ function switchStopwatch() {
 function stop() {
   status = "switchedOff";
   replaceClass(startBtn.firstChild, "fa-arrows-spin", "fa-play");
-  stopwatch.textContent = "00:00:00";
-  time.textContent = "00:00:00";
+  stopwatch.textContent = "00:00,00";
+  lapNumber = 1;
+  timeList.replaceChildren();
 }
 function pause() {
   status = "paused";
@@ -56,20 +61,20 @@ function count() {
     if (status === "switchedOn") {
       currentTime = new Date();
       demonstratedTime = new Date(currentTime - startTime);
-      showTime(demonstratedTime, stopwatch);
+      showTime(stopwatch, "", demonstratedTime);
     } else if (status === "paused") {
       pausedTime = demonstratedTime;
       return;
     }
   });
 }
-function showTime(date, element) {
+function showTime(element, description, date) {
   const min = date.getMinutes().toString().padStart(2, 0);
   const sec = date.getSeconds().toString().padStart(2, 0);
   const ms = Math.trunc(date.getMilliseconds() / 10)
     .toString()
     .padStart(2, 0);
-  element.textContent = `${min}:${sec}:${ms}`;
+  element.textContent = `${description}${min}:${sec},${ms}`;
 }
 
 function replaceClass(element, oldClass, newClass) {
